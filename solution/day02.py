a63d7513f5844e474992a17c41f5e26d82f8c255
whil0012 <whil0012@users.noreply.github.com>
2017-12-08 13:43:18 -0500

    Day 02 - Part 2 solution

--- solution/day02.py
+++ solution/day02.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import itertools
 
 
 class TestDay02(unittest.TestCase):
@@ -14,13 +15,26 @@ class TestDay02(unittest.TestCase):
         actual = get_row_difference(test_row_numbers)
         self.assertEquals(actual, 5)
 
-    def test_get_checksum_whenThreeRowsDifference8And4And6_returns18(self):
+    def test_get_checksum_difference_whenThreeRowsDifference8And4And6_returns18(self):
         test_row_1 = [5, 1, 9, 5]
         test_row_2 = [7, 5, 3]
         test_row_3 = [2, 4, 6, 8]
         test_rows = [test_row_1, test_row_2, test_row_3]
-        actual = get_checksum(test_rows)
+        actual = get_checksum_difference(test_rows)
         self.assertEquals(actual, 18)
+
+    def test_get_row_division_when9And3_returns3(self):
+        test_row_numbers = [9, 4, 7, 3]
+        actual = get_row_division(test_row_numbers)
+        self.assertEquals(actual, 3)
+
+    def test_get_checksum_division_whenThreeRowsDivision4And3And2_returns9(self):
+        test_row_1 = [5, 9, 2, 8]
+        test_row_2 = [9, 4, 7, 3]
+        test_row_3 = [3, 8, 6, 5]
+        test_rows = [test_row_1, test_row_2, test_row_3]
+        actual = get_checksum_division(test_rows)
+        self.assertEquals(actual, 9)
 
 
 def get_spreadsheet_row_numbers(row):
@@ -32,10 +46,25 @@ def get_row_difference(row_numbers):
     max_number = max(row_numbers)
     return max_number - min_number
 
-def get_checksum(rows):
+
+def get_row_division(row_numbers):
+    combinations = itertools.combinations(row_numbers, 2)
+    item = [x for x in combinations if (x[0] % x[1] == 0) or (x[1] % x[0] == 0)]
+    return int(max(item[0]) / min(item[0]))
+
+
+def get_checksum_difference(rows):
+    return get_checksum(rows, get_row_difference)
+
+
+def get_checksum_division(rows):
+    return get_checksum(rows, get_row_division)
+
+
+def get_checksum(rows, row_checksum_func):
     result = 0
     for row in rows:
-        difference = get_row_difference(row)
+        difference = row_checksum_func(row)
         result += difference
     return result
 
@@ -47,8 +76,10 @@ def main():
         for line in input_file:
             row = get_spreadsheet_row_numbers(line)
             rows.append(row)
-    checksum = get_checksum(rows)
+    checksum = get_checksum_difference(rows)
     print('checksum: ', checksum)
+    checksum = get_checksum_division(rows)
+    print('checksum division: ', checksum)
 
 
 if __name__ == '__main__':
