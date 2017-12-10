a8898d32e5d65acde938887bb5bee15d741e1a6e
whil0012 <whil0012@users.noreply.github.com>
2017-12-10 11:27:10 -0500

    Day 06 - Part 2 solution

--- solution/day06.py
+++ solution/day06.py
@@ -2,6 +2,10 @@ import unittest
 import os
 from operator import itemgetter
 from input_utilities.inputfilepath import get_input_file_path
+from collections import namedtuple
+
+
+ReallocationCycle = namedtuple('ReallocationCycle', ['from_start', 'cycle_count'])
 
 
 class TestDay06(unittest.TestCase):
@@ -35,30 +39,43 @@ class TestDay06(unittest.TestCase):
         actual = reallocate_blocks(blocks)
         self.assertListEqual(actual, [1, 3, 4, 1])
 
-    def test_find_duplicate_reallocation_cycle_count_when0270_returns5(self):
+    def test_find_duplicate_reallocation_cycle_count_when0270_returns_from_start_5(self):
         blocks = [0, 2, 7, 0]
-        actual = find_duplicate_reallocation_cycle_count(blocks)
-        self.assertEquals(actual, 5)
+        actual = find_duplicate_reallocation_cycle(blocks)
+        self.assertEquals(actual.from_start, 5)
+
+    def test_find_duplicate_reallocation_cycle_count_when0270_returns_cycle_count_4(self):
+        blocks = [0, 2, 7, 0]
+        actual = find_duplicate_reallocation_cycle(blocks)
+        self.assertEquals(actual.cycle_count, 4)
 
 
-def add_blocks_to_history(blocks, block_history):
-    block_history.add(tuple(blocks))
+def add_blocks_to_history(blocks, reallocation_count, block_history):
+    block_history[tuple(blocks)] = reallocation_count
 
 
-def blocks_in_block_history(blocks, block_history):
-    return tuple(blocks) in block_history
+def find_block_in_block_history(blocks, block_history):
+    if tuple(blocks) in block_history:
+        return (blocks, block_history[tuple(blocks)])
+    return None
 
 
-def find_duplicate_reallocation_cycle_count(blocks):
-    block_history = set()
-    add_blocks_to_history(blocks, block_history)
+def create_new_block_history(blocks):
+    return {tuple(blocks): 0}
+
+
+def find_duplicate_reallocation_cycle(blocks):
+    block_history = create_new_block_history(blocks)
     reallocation_count = 1
     new_blocks = reallocate_blocks(blocks)
-    while not blocks_in_block_history(new_blocks, block_history):
-        add_blocks_to_history(new_blocks, block_history)
+    found_block = find_block_in_block_history(new_blocks, block_history)
+    while found_block is None:
+        add_blocks_to_history(new_blocks, reallocation_count, block_history)
         reallocation_count += 1
         new_blocks = reallocate_blocks(new_blocks)
-    return reallocation_count
+        found_block = find_block_in_block_history(new_blocks, block_history)
+    cycle_count = reallocation_count - found_block[1]
+    return ReallocationCycle(reallocation_count, cycle_count)
 
 
 def reallocate_blocks(blocks):
@@ -88,8 +105,9 @@ def main():
     with open(file_path) as input_file:
         line = input_file.readline()
         blocks = list(map(int, line.split()))
-    reallocation_count = find_duplicate_reallocation_cycle_count(blocks)
-    print('reallocation count: ', reallocation_count)
+    reallocation_cycle = find_duplicate_reallocation_cycle(blocks)
+    print('reallocation count: ', reallocation_cycle.from_start)
+    print('infinite cycle count: ', reallocation_cycle.cycle_count)
 
 
 if __name__ == '__main__':
