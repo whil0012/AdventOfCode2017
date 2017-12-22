19982d89ced2e324ba9ba766d713be0740221801
whil0012 <whil0012@users.noreply.github.com>
2017-12-22 14:24:06 -0500

    Day 08 - Part 1 - completed solution

--- solution/day08.py
+++ solution/day08.py
@@ -77,16 +77,126 @@ class TestDay08(unittest.TestCase):
         actual = create_instruction('b dec 5 if a != 42')
         self.assertEquals(actual.condition_value, 42)
 
-    def test_process_instruction_whenConditionNotMet_doesNothing(self):
-        registers = {}
-        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.EqualTo, 42)
+    def test_process_instruction_whenConditionNotMetAndRegisterInRegisters_doesNothing(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.GreaterThan, 42)
         process_instruction(instruction, registers)
-        expected_registers = {}
+        expected_registers = {'a': 1, 'b':1}
         self.assertDictEqual(registers, expected_registers)
+
+    def test_process_instruction_whenIncrementGTAndRegisterInRegisters_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.GreaterThan, 0)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementLTAndRegisterInRegisters_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.LessThan, 2)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementETAndRegisterInRegisters_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.EqualTo, 1)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementGETAndGreaterThan_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.GreaterThanEqualTo, 0)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementGETAndEqualTo_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.GreaterThanEqualTo, 1)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementLETAndLessThan_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.LessThanEqualTo, 2)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementLETAndEqualTo_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.LessThanEqualTo, 1)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenIncrementNET_incrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Increment, 5, 'a', ComparisonOperator.NotEqualTo, 2)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, 6)
+
+    def test_process_instruction_whenDecrement_decrementsRegister(self):
+        registers = {'a': 1, 'b':1}
+        instruction = Instruction('b', Operation.Decrement, 5, 'a', ComparisonOperator.GreaterThan, 0)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, -4)
+
+    def test_process_instruction_whenConditionRegisterNotInRegisters_Uses0ForConditionRegisterValue(self):
+        registers = {'b':1}
+        instruction = Instruction('b', Operation.Decrement, 5, 'a', ComparisonOperator.LessThan, 1)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, -4)
+
+    def test_process_instruction_whenOperationRegisterNotInRegisters_Uses0ForOperationRegisterValue(self):
+        registers = {'a': 1}
+        instruction = Instruction('b', Operation.Decrement, 5, 'a', ComparisonOperator.GreaterThan, 0)
+        process_instruction(instruction, registers)
+        actual = registers['b']
+        self.assertEquals(actual, -5)
+
+
+def instruction_condition_true(condition_register_value, condition_operator, condition_value):
+    if condition_operator == ComparisonOperator.GreaterThan:
+        return condition_register_value > condition_value
+    elif condition_operator == ComparisonOperator.LessThan:
+        return condition_register_value < condition_value
+    elif condition_operator == ComparisonOperator.EqualTo:
+        return condition_register_value == condition_value
+    elif condition_operator == ComparisonOperator.GreaterThanEqualTo:
+        return condition_register_value >= condition_value
+    elif condition_operator == ComparisonOperator.LessThanEqualTo:
+        return condition_register_value <= condition_value
+    elif condition_operator == ComparisonOperator.NotEqualTo:
+        return condition_register_value != condition_value
+
+
+def process_instruction_operation(register_operand_value, operation, value_operand):
+    if operation == Operation.Increment:
+        return register_operand_value + value_operand
+    else:
+        return register_operand_value - value_operand
+
+
+def get_register_value(registers, condition_register):
+    if condition_register in registers.keys():
+        return registers[condition_register]
+    else:
+        return 0
 
 
 def process_instruction(instruction, registers):
-    pass
+    condition_register_value = get_register_value(registers, instruction.condition_register)
+    if instruction_condition_true(condition_register_value, instruction.condition_operator, instruction.condition_value):
+        register_operand_value = get_register_value(registers, instruction.register_operand)
+        new_value = process_instruction_operation(register_operand_value, instruction.operation, instruction.value_operand)
+        registers[instruction.register_operand] = new_value
 
 
 def create_instruction(instruction_description):
@@ -138,3 +248,18 @@ def get_operation(instruction_parts):
 
 def get_register_operand(instruction_parts):
     return instruction_parts[0]
+
+
+def main():
+    registers = {}
+    file_path = get_input_file_path('day08.txt')
+    with open(file_path, 'r') as input_file:
+        for line in input_file:
+            instruction = create_instruction(line.strip())
+            process_instruction(instruction, registers)
+    max_register_value = max(registers.values())
+    print("max register value: ", max_register_value)
+
+
+if __name__ == '__main__':
+    main()
