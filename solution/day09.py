1144dae77be004d137c44df426763dce6154908a
whil0012 <whil0012@users.noreply.github.com>
2017-12-22 15:38:37 -0500

    Day 09 - Part 2 - Solution

--- solution/day09.py
+++ solution/day09.py
@@ -5,36 +5,53 @@ from input_utilities.inputfilepath import get_input_file_path
 class TestDay09(unittest.TestCase):
     def test_score_groups_whenOneGroup_returns1(self):
         actual = score_groups('{}')
-        self.assertEquals(actual, 1)
+        self.assertEquals(actual[0], 1)
 
     def test_score_groups_whenTwoGroupsNotNested_returns2(self):
         actual = score_groups('{}{}')
-        self.assertEquals(actual, 2)
+        self.assertEquals(actual[0], 2)
 
     def test_score_groups_whenTwoGroupsOneNested_returns3(self):
         actual = score_groups('{{}}')
-        self.assertEquals(actual, 3)
+        self.assertEquals(actual[0], 3)
 
     def test_score_groups_whenThreeGroupsNestedAtOneLevel_returns5(self):
         actual = score_groups('{{}{}}')
-        self.assertEquals(actual, 5)
+        self.assertEquals(actual[0], 5)
 
     def test_score_groups_whenOneGroupNestedIsInGarbage_returns1(self):
         actual = score_groups('{<{}>}')
-        self.assertEquals(actual, 1)
+        self.assertEquals(actual[0], 1)
 
     def test_score_groups_whenOneGroupInGarbageOneNotIn_returns3(self):
         actual = score_groups('{<{}>{}}')
-        self.assertEquals(actual, 3)
+        self.assertEquals(actual[0], 3)
 
     def test_score_groups_whenFirstClosingGarbageCharacterIgnored_ignoresFirstClosingCharacter(self):
         actual = score_groups('{<{!>{}>}')
-        self.assertEquals(actual, 1)
+        self.assertEquals(actual[0], 1)
+
+    def test_score_groups_whenNoGarbageGroups_returns0(self):
+        actual = score_groups('{{{}}}')
+        self.assertEquals(actual[1], 0)
+
+    def test_score_groups_whenEmptyGabageGroup_returns0(self):
+        actual = score_groups('{<>}')
+        self.assertEquals(actual[1], 0)
+
+    def test_score_groups_when3GarbageCharacters_returns3(self):
+        actual = score_groups('{<123>}')
+        self.assertEquals(actual[1], 3)
+
+    def test_score_groups_whenCancelledCharacters_doesNotCountCancelledCharacters(self):
+        actual = score_groups('{<12!3>}')
+        self.assertEquals(actual[1], 2)
 
 
 def score_groups(input_text):
     score = 0
     level = 0
+    garbage_characters_count = 0
     in_garbage = False
     ignore_next_character = False
     for character in input_text:
@@ -46,6 +63,8 @@ def score_groups(input_text):
                 ignore_next_character = True
             elif character == '>':
                 in_garbage = False
+            else:
+                garbage_characters_count += 1
             continue
         if character == '<':
             in_garbage = True
@@ -54,7 +73,7 @@ def score_groups(input_text):
             score += level
         elif character == '}':
             level -= 1
-    return score
+    return (score, garbage_characters_count)
 
 
 def main():
@@ -62,7 +81,8 @@ def main():
     with open(file_path, 'r') as input_file:
         input_text = input_file.read()
     groups_score = score_groups(input_text)
-    print('groups score: ', groups_score)
+    print('groups score: ', groups_score[0])
+    print('garbage characters count: ', groups_score[1])
 
 
 if __name__ == '__main__':
