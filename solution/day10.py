5f6b3de72972019b85fa0c2320d3c255ecc2a763
whil0012 <whil0012@users.noreply.github.com>
2017-12-22 15:44:05 -0500

    Day 10 - Part 1 - Description, input file, and empty, failing test

---
+++ solution/day10.py
@@ -0,0 +1,7 @@
+import unittest
+from input_utilities.inputfilepath import get_input_file_path
+
+
+class TestDay10(unittest.TestCase):
+    def test_empty_test_fails(self):
+        self.assertFalse(True, 'empty test')
